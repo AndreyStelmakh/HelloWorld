7c98b0fe0ad3316c003281a6049392ba27f67fda
AndreyStelmakh <andrey.stelmakh@live.ru>
2015-11-25 22:24:01 +0300

    Ура

---
+++ Solution/Project_Library/Calc.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace F.Project_Library
+{
+    public class Calc
+    {
+        static public double AreaOfTriangle(double l1, double l2, double l3)
+        {
+            #region Определяем кто есть кто, и проверки
+
+            if(l1 <= 0 || l2 <= 0 || l3 <= 0)
+            {
+                throw new ArgumentOutOfRangeException();
+
+            }
+
+            // в "c" - вероятная гипотенуза
+            double a, b, c; 
+            if(l1 > l2 && l1 > l3)
+            {
+                c = l1;
+                a = l2;
+                b = l3;
+
+            }
+            else if(l2 > l3)
+            {
+                c = l2;
+                a = l1;
+                b = l3;
+
+            }
+            else
+            {
+                c = l3;
+                a = l1;
+                b = l2;
+
+            }
+
+            if( a*a + b*b != c*c)
+            {
+                throw new ArgumentOutOfRangeException("Треугольник не прямоугольный или проблемы с точностью");
+
+            }
+
+            #endregion
+
+            return a * b / 2;
+
+        }
+
+    }
+
+}
---
+++ Solution/Project_UnitTests/Calc__Tests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using F.Project_Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F.Project_Library.Tests
+{
+    [TestClass()]
+    public class Calc__Tests
+    {
+        [TestMethod()]
+        public void AreaOfTriangle__Test()
+        {
+            StringBuilder fails = new StringBuilder();
+            fails.AppendLine();
+            bool failed = false;
+
+            var testArgsList = new List<Tuple<double, double, double>>();
+
+            testArgsList.Add(new Tuple<double, double, double>(0, 0, 0));
+            testArgsList.Add(new Tuple<double, double, double>(-1, -1, -1));
+            testArgsList.Add(new Tuple<double, double, double>(-1, 1, 1));
+            testArgsList.Add(new Tuple<double, double, double>(-1, 1, 1));
+
+            foreach (var testArgs in testArgsList)
+            {
+                try
+                {
+                    Calc.AreaOfTriangle(testArgs.Item1, testArgs.Item2, testArgs.Item3);
+
+                    failed = true;
+
+                    fails.AppendLine(string.Format("Тест не прошедши {0}", testArgs.ToString()));
+
+                }
+                catch { }
+
+            }
+
+            if(failed)
+            {
+                Assert.Fail(fails.ToString());
+
+            }
+
+        }
+
+    }
+
+}
